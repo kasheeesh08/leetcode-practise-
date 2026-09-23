d29e2ade53d8d9ca1aeb15e8b81b1efc53b45c42
kasheeesh08 <kashishhome123@gmail.com>
2026-09-23 12:54:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (44.15%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-     return pow(x,n);   
+
+        //edge case : converting negative into positive will lead to overflow that is why we will take long long 
+
+        double ans = 1.0;
+        long long dup_n = n;
+        if(dup_n < 0)
+            dup_n = -1*dup_n;
+
+        //logic for positive num as power 
+        while(dup_n){
+            //even number as power
+            if(dup_n%2 == 0){
+                x = x*x;
+                dup_n = dup_n/2;
+            }
+            //odd number as power
+            else{
+                ans = ans*x;
+                dup_n = dup_n -1;
+            }
+        }
+        //logic for negative num as power 
+        if(n < 0){
+            ans = (double)(1.0)/(double)(ans);
+        }
+        return ans;
     }
+    
 };
